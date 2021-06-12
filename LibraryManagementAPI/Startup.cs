dd5e5f720bc81712b552bd7735ae8efbcfe8b7b1
Vivek Prakash <vivekprakash78@gmail.com>
2021-06-13 01:37:57 +0530

    Added DI for AppSettings and Database

--- LibraryManagementAPI/Startup.cs
+++ LibraryManagementAPI/Startup.cs
@@ -1,16 +1,13 @@
+using Common;
+using DatabaseService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LibraryManagementAPI
 {
@@ -26,12 +23,17 @@ namespace LibraryManagementAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DatabaseContext>(opt => opt.UseInMemoryDatabase(databaseName: "Library"));
+            services.AddScoped<IDatabaseRepository, DatabaseRepository>();
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "LibraryManagementAPI", Version = "v1" });
             });
+
+            services.Configure<AppSettings>(Configuration.GetSection(nameof(AppSettings)));
+            services.AddSingleton(resolver => resolver.GetRequiredService<IOptionsMonitor<AppSettings>>().CurrentValue);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +45,8 @@ namespace LibraryManagementAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LibraryManagementAPI v1"));
             }
+
+            AppSettingsHelper.Services = app.ApplicationServices;
 
             app.UseHttpsRedirection();
 
